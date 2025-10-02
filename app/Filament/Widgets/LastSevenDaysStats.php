1fc4ef5a2fdb9765ed10bcd1b2d45e78125f69ac
fatmatawfeek <fatmatawfeek97@gmail.com>
2025-10-02 20:27:39 +0300

    Dashboard with Stats

--- app/Filament/Widgets/LastSevenDaysStats.php
+++ app/Filament/Widgets/LastSevenDaysStats.php
@@ -2,15 +2,20 @@
 
 namespace App\Filament\Widgets;
 
+use App\Models\Property;
+use App\Models\User;
 use Filament\Widgets\StatsOverviewWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
 class LastSevenDaysStats extends StatsOverviewWidget
 {
+    protected int|array|null $columns = 2;
+
     protected function getStats(): array
     {
         return [
-            //
+            Stat::make('Last 7 days new properties', Property::where('created_at', '>', now()->subDays(7)->endOfDay())->count()),
+            Stat::make('Last 7 days new users', User::where('created_at', '>', now()->subDays(7)->endOfDay())->count()),
         ];
     }
 }
